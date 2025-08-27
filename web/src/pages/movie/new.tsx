93bb91aa3eb31a6b7653b0de368a9e909cb6ec25
nicholaslouback <nilouback@hotmail.com>
2025-08-27 10:52:38 -0300

    fix: ajusting route

--- web/src/pages/movie/new.tsx
+++ web/src/pages/movie/new.tsx
@@ -1,7 +1,11 @@
+import { ROUTES } from "@/routes/routes";
+import Link from "next/link";
+
 export default function NewMoviePage() {
   return (
     <div>
       <h1>Adicionar Novo Filme</h1>
+      <Link href={ROUTES.movie.new.path}>Adicionar Novo Filme</Link>
     </div>
   )
 }
